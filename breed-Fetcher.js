e837d6015a78fb968a954a9ee29255e1948e1acc
Wanjin Yoo <dksladjsla@gmail.com>
2020-11-06 03:18:38 +0000

    test case && callback functions are created

--- breed-Fetcher.js
+++ breed-Fetcher.js
@@ -1,12 +1,18 @@
 const request = require('request');
 const searchval = process.argv[2];
-const address = `https://api.thecatapi.com/v1/breeds/search?api_key=d9852f75-031f-4d41-9f84-0dcceb663e55&q=${searchval}`;
 
-request(address, (error, response, body) => {
-  if (error) console.log(`Invalid dog`);
-  let data = JSON.parse(body);
-  if (data.length === 0) console.log(`cat not found`);
-  else console.log(data[0][`description`]);
-}
-);
 
+
+
+const fetchBreedDescription = function(breedName, callback) {
+  const address = `https://api.thecatapi.com/v1/breeds/search?api_key=d9852f75-031f-4d41-9f84-0dcceb663e55&q=${breedName}`;
+  request(address, (error, response, body) => {
+    if (error) callback(`error occured`,data);
+    let data = JSON.parse(body);
+    if (data.length === 0) callback(`error occured`,data);
+    else callback(null,data[0][`description`]);
+  }
+  );
+};
+
+module.exports = {fetchBreedDescription};
